12cce68e9365bece3c9f1cf19e6281c87602ff74
Bjoern Siepe <bjoernsiepe@gmail.com>
2023-12-20 14:27:41 +0100

    add centrality functions
    these allow for extraction and plotting, will later be added to tsnet

---
+++ revision1/get_centrality.R
@@ -0,0 +1,47 @@
+
+# -------------------------------------------------------------------------
+# Idea:
+# Write one function that obtains centrality measures from posterior arrays
+# The one function that plots these centrality measures
+# currently supports instrength, outstrength, density
+# Things to think about: does this also work with the Stan samples?
+
+get_centrality <- function(fitobj,
+                           burnin = 500){
+  
+  
+  #--- BGGM
+  # Obtain samples
+  beta_samps <- abs(fitobj$fit$beta[,, -c(1:burnin)])
+  pcor_samps <- abs(fitobj$fit$pcors[,, -c(1:burnin)])
+  
+  cnames <- colnames(fitobj$Y)
+  
+  #--- Centrality measures
+  # In-strength
+  instrength <- t(apply(beta_samps, MARGIN = 3, FUN = rowSums))
+  colnames(instrength) <- cnames
+  
+  # Out-strength
+  outstrength <- t(apply(beta_samps, MARGIN = 3, FUN = colSums))
+  colnames(outstrength) <- cnames
+  
+  # Contemporaneous strength
+  strength <- t(apply(pcor_samps, MARGIN = 3, FUN = rowSums))
+  colnames(strength) <- cnames
+  
+  # Density
+  density_beta <- apply(beta_samps, MARGIN = 3, FUN = sum)
+  density_pcor <- apply(pcor_samps, MARGIN = 3, FUN = sum)
+  
+  #--- Return
+  return(list(instrength = instrength,
+              outstrength = outstrength,
+              strength = strength,
+              density_beta = density_beta,
+              density_pcor = density_pcor))
+  
+}
+
+
+
---
+++ revision1/plot_centrality.R
@@ -0,0 +1,108 @@
+plot_centrality <- function(obj,
+                            # accepts overview or density
+                            plot_type = "tiefighter",
+                            # accepts: instrength, outstrength, strength, density_beta, density_pcor
+                            density_type = "outstrength",
+                            # only accepts one for now
+                            cis = 0.95){
+  
+  
+  
+  
+  if (!is.numeric(cis) || any(cis <= 0) || any(cis >= 1)) {
+    stop("cis must be a numeric vector with values between 0 and 1 (exclusive)")
+  }
+  
+  
+  #--- Preparation
+  # Combine all centrality measures
+  create_centrality_df <- function(centrality, suffix) {
+    df <- as.data.frame(centrality)
+    colnames(df) <- paste0(colnames(df), "_", suffix)
+    return(df)
+  }
+  
+  instrength <- create_centrality_df(obj$instrength, "instrength")
+  outstrength <- create_centrality_df(obj$outstrength, "outstrength")
+  strength <- create_centrality_df(obj$strength, "strength")
+ 
+  
+  df_centrality <- cbind(instrength, 
+                         outstrength, 
+                         strength)
+  
+  #--- Overview
+  if (plot_type == "tiefighter") {
+     
+    #--- Plot
+    overview_plot <- df_centrality %>% 
+      tidyr::pivot_longer(cols = everything(),
+                          names_to = "measure",
+                          values_to = "value") |>
+      dplyr::group_by(.data$measure) |>
+      dplyr::summarize(mean_value = mean(.data$value, 
+                                      na.rm = TRUE),
+                    lb = quantile(.data$value, 
+                                  probs = (1 - cis) / 2,
+                                  na.rm = TRUE),
+                    ub = quantile(.data$value,
+                                  probs = 1 -((1 - cis) / 2)),
+                                  na.rm = TRUE) |>
+      dplyr::ungroup() |>
+      tidyr::separate(col = measure,
+                      into = c("variable", "centrality"),
+                      sep = "_") |>
+      ggplot2::ggplot(aes(x = .data$mean_value, y = .data$variable)) +  
+      ggplot2::geom_point() + 
+      ggplot2::geom_errorbarh(aes(
+        xmin = lb , 
+        xmax = ub)) +
+      ggdist::theme_ggdist() + 
+      ggplot2::facet_grid(. ~ .data$centrality) + 
+      labs(x = "Centrality",
+           y = "Variable")
+
+      return(overview_plot)
+  
+  }
+  #--- Density
+  if(plot_type == "density"){
+    
+
+    #--- Plot
+    browser()
+    density_plot <- df_centrality %>% 
+      tidyr::pivot_longer(cols = everything(),
+                          names_to = "measure",
+                          values_to = "value") |>
+      dplyr::group_by(.data$measure) |>
+      dplyr::mutate(mean_value = mean(.data$value, 
+                                         na.rm = TRUE)) |>
+      dplyr::ungroup() |>
+      tidyr::separate(col = measure,
+                      into = c("variable", "centrality"),
+                      sep = "_") |>
+      ggplot2::ggplot(aes(
+                          x = .data$value, 
+                          y = .data$variable)) +  
+      ggdist::stat_slab(aes(
+                        fill = after_stat(.data$level)), 
+                        # fixed width for now
+                        .width = c(0.8, 0.9, 0.95, 1)) +
+      ggdist::stat_pointinterval(aes(x = .data$mean_value), 
+                                 size = 1) +
+      scale_alpha(guide = "none") +
+      ggdist::theme_ggdist() + 
+      ggplot2::facet_grid(. ~ .data$centrality) + 
+      scale_fill_brewer() + 
+      labs(x = "Centrality",
+           y = "Variable", 
+           fill = "Credible Interval")
+    
+    return(density_plot)
+    
+    
+    
+    
+  }
+}  
